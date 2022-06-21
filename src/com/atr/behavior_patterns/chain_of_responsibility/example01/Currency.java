0cda35fcd7eb9dafc8e2dfaee16e884a48f12cb5
Adrian <adrian.talonia11@gmail.com>
2022-06-20 21:19:14 -0500

    chain resposability example01

---
+++ src/com/atr/behavior_patterns/chain_of_responsibility/example01/Currency.java
@@ -0,0 +1,13 @@
+package com.atr.behavior_patterns.chain_of_responsibility.example01;
+
+public class Currency {
+    private int amount;
+
+    public Currency(int amt) {
+        amount = amt;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+}
---
+++ src/com/atr/behavior_patterns/chain_of_responsibility/example01/DispenseChain.java
@@ -0,0 +1,79 @@
+package com.atr.behavior_patterns.chain_of_responsibility.example01;
+
+interface DispenseChain {
+    void setNextChain(DispenseChain nextChain);
+
+    void dispense(Currency currency);
+}
+
+class Dollar50Dispenser implements DispenseChain {
+    private DispenseChain chain;
+
+    @Override
+    public void setNextChain(DispenseChain nextChain) {
+        chain = nextChain;
+    }
+
+    @Override
+    public void dispense(Currency currency) {
+        if (currency.getAmount() >= 50) {
+            int num = currency.getAmount() / 50;
+            int remainder = currency.getAmount() % 50;
+            System.out.println("Dispensing " + num + " 50$ note");
+
+            if (remainder != 0) {
+                this.chain.dispense(new Currency(remainder));
+            }
+        } else {
+            this.chain.dispense(currency);
+        }
+    }
+}
+
+class Dollar20Dispenser implements DispenseChain {
+    private DispenseChain chain;
+
+    @Override
+    public void setNextChain(DispenseChain nextChain) {
+        chain = nextChain;
+    }
+
+    @Override
+    public void dispense(Currency currency) {
+        if (currency.getAmount() >= 20) {
+            int num = currency.getAmount() / 20;
+            int remainder = currency.getAmount() % 20;
+            System.out.println("Dispensing " + num + " 20$ note");
+
+            if (remainder != 0) {
+                this.chain.dispense(new Currency(remainder));
+            }
+        } else {
+            this.chain.dispense(currency);
+        }
+    }
+}
+
+class Dollar10Dispenser implements DispenseChain {
+    private DispenseChain chain;
+
+    @Override
+    public void setNextChain(DispenseChain nextChain) {
+        chain = nextChain;
+    }
+
+    @Override
+    public void dispense(Currency currency) {
+        if (currency.getAmount() >= 10) {
+            int num = currency.getAmount() / 10;
+            int remainder = currency.getAmount() % 10;
+            System.out.println("Dispensing " + num + " 10$ note");
+
+            if (remainder != 0) {
+                this.chain.dispense(new Currency(remainder));
+            }
+        } else {
+            this.chain.dispense(currency);
+        }
+    }
+}
