c3c0f97ff45ec1d53af72ee0a9c83783e1d6fb7a
Adrian <adrian.talonia11@gmail.com>
2022-07-22 17:23:10 -0500

    challenge

---
+++ src/com/atr/behavior_patterns/strategy/challenge/Strategy.java
@@ -0,0 +1,26 @@
+package com.atr.behavior_patterns.strategy.challenge;
+
+public interface Strategy {
+    public int doOperation(int num1, int num2);
+}
+
+class OperationAdd implements Strategy {
+    @Override
+    public int doOperation(int num1, int num2) {
+        return num1 + num2;
+    }
+}
+
+class OperationSubstract implements Strategy {
+    @Override
+    public int doOperation(int num1, int num2) {
+        return num1 - num2;
+    }
+}
+
+class OperationMultiply implements Strategy {
+    @Override
+    public int doOperation(int num1, int num2) {
+        return num1 * num2;
+    }
+}
